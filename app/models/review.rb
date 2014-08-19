11390750da688ca440a29b0062105797c95d1ae3
shaher nader <eng25shaher@gmail.com>
2014-08-19 09:37:57 +0300

    final

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,6 @@ class Review < ActiveRecord::Base
 	validates :quilty, :presence => true
 	validates :disicpline, :presence => true
 	validates :treatment, :presence => true
-	validates :client_name, :presence => true
-	validates :client_phone, :presence => true
-	validates :client_email, :presence => true
+
 	belongs_to :worker
 end
